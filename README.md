51cd119c09718c0a68578f25cc87101e7a3e4b97
SirJubilant <67059552+SirJubilant@users.noreply.github.com>
2023-12-22 11:45:11 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# getLinked
+ My submission to the getLinked Hackathon
---
+++ app.js
@@ -0,0 +1,48 @@
+    const mobileMenu = document.querySelector('.menu_bar');
+    const navLinks = document.querySelector('.nav_list');
+    const closeMenu = document.querySelector('.close_bar');
+    const navLinkList = document.querySelectorAll('.list_item');
+
+    mobileMenu.addEventListener('click', ()=> {
+        // opens the navbar and also controls the display of the menu and close bars
+        openMobileMenu();
+        closeMenu.classList.add('menu_active');
+        mobileMenu.classList.remove('menu_active');
+    });
+
+    closeMenu.addEventListener('click', () => {
+         // closes the navbar and also controls the display of the menu and close bars
+        closeMobileMenu();
+        mobileMenu.classList.add('menu_active');
+        closeMenu.classList.remove('menu_active');
+    });
+
+    // the open Mobile menu function and animations
+    function openMobileMenu() {
+        navLinks.classList.add('active');
+        navLinkList.forEach((link, index) => {
+            link.classList.add('fade-in');
+            link.style.animationDelay = `${index * 0.1}s`;
+
+            //remove the animation when it ends
+            link.addEventListener('animationend', () => {
+                link.classList.remove('fade-in')
+            });
+        });
+    }
+
+// the close Mobile menu function and animations
+    function closeMobileMenu() {
+        
+        navLinkList.forEach((link, index) => {
+            link.classList.add('fade-out');
+            link.style.animationDelay = `${index * 0.1}s`;
+            navLinks.classList.remove('active');
+            
+            
+             //remove the animation when it ends
+             link.addEventListener('animationend', () => {
+                link.classList.remove('fade-out')
+            });
+        });
+    }
